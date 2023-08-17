da535430ed538c820f9f8ee9b519476d953c051d
SauBanh <saubanh05062001@gmail.com>
2023-08-17 14:10:48 +0700

    get all post

--- API/routers/post.py
+++ API/routers/post.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm.session import Session
 from db.database import get_db
 from routers.schemas import PostDisplay, PostBase
 from db import db_post
+from typing import List
 
 router = APIRouter(
     prefix='/post',
@@ -17,3 +18,7 @@ def create(request: PostBase, db: Session = Depends(get_db)):
     if not request.image_url_type in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Pramater image_url_type can only take value 'absolute' or 'relative'")
     return db_post.create(db, request)
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
